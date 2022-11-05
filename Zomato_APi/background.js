651b824d7c5fe6157bef1b6b583e21aafc210f34
Nk <nithishkumar.appscomp@gmail.com>
2022-11-05 12:13:56 +0530

    [Add] Added onclick listener code

---
+++ Zomato_APi/background.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function() {
+  document.addEventListener("click", function (e) {
+    console.log("Clicked");
+//    alert("Clicked");
+  },{once : true});
+});
+
+
+/*
+window.onclick = function(event) {
+    var target = event.target ;
+    if(target.matches('#clickactivity')) {
+        var clickedEle = document.activeElement.id ;
+        var ele = document.getElementById(clickedEle);
+        alert(ele.text);
+    }
+}
+*/
--- Zomato_APi/popup.js
+++ Zomato_APi/popup.js
@@ -1,7 +1,7 @@
 function injectTheScript() {
     // Wuery the active tab, which will be only one tab and inject the script in it.
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['content_script.js']})
+        chrome.scripting.executeScript({target: {tabId: tabs[0].id}, files: ['inject.js']})
     })
 }
 
