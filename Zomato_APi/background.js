9ce8c36468b777a9fb85320b8efcd81d666d348e
Nithin Gunasekaran <nithin@appscomp.com>
2022-12-07 11:41:05 +0530

    Update code to use sockets

--- Zomato_APi/background.js
+++ Zomato_APi/background.js
@@ -1,27 +1,54 @@
 var $j = jQuery.noConflict();
 
+async function getUserId() {
+    const data = await fetch('https://www.zomato.com/restaurant-onboard-diy/check-auth')
+        .then((response) => response.json())
+    return data.userId
+}
+
+async function getRestaurantIds() {
+    const data = await fetch('https://www.zomato.com/merchant-api/home/secondary-config')
+        .then((response) => response.json())
+    return Object.keys(data.restaurants_config)
+}
+
 $j(document).ready(function () {
-    function getZomato() {
-        fetch('https://www.zomato.com/merchant-api/v3/notifications/fetch')
-            .then((response) => response.json())
-            .then((data) => {
-            console.log("The response data is here", data)
-                fetch('https://pos.appscomp.in/api/login', {
-                    method: 'POST',
-                    mode: 'no-cors',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(data),
-                })
+    async function getZomato() {
+        console.log("I am in zomato land")
+        const socket = io("https://cc2.zomato.com/");
+        const user = await getUserId();
+        const restaurantIds = await getRestaurantIds();
+        console.log(user);
+        console.log(restaurantIds);
+        socket.connect();
+        socket.on('hello', () => {
+            console.log("In hello");
+            socket.emit('yello', {
+                user,
+                userId: user,
+                resIds: restaurantIds,
+                client: 'web',
+                version: '2',
             });
+        });
+
+        socket.on('res_order', data => {
+            console.log("Received order")
+            fetch('https://pos.appscomp.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+        });
     }
 
     function getSwiggy() {
         fetch('https://partner.swiggy.com/orders/v1/fetch')
             .then((response) => response.json())
             .then((data) => {
-            console.log("The response data is here", data)
+                console.log("The response data is here", data)
                 fetch('https://pos.appscomp.in/api/login', {
                     method: 'POST',
                     headers: {
