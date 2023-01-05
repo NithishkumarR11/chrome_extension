d7bbc0a5ee1540ce5120aa808b70aa1aa8331b8b
Nk <nithishkumar.appscomp@gmail.com>
2023-01-05 16:25:18 +0530

    [IMPROVE] Tab id code has been added

--- Zomato_APi/background.js
+++ Zomato_APi/background.js
@@ -17,9 +17,9 @@ $j(document).ready(function () {
         console.log("I am in zomato land")
         const socket = io("https://cc2.zomato.com/");
         const user = await getUserId();
+        console.log("The user id is displayed", user)
         const restaurantIds = await getRestaurantIds();
-        console.log(user);
-        console.log(restaurantIds);
+        console.log("The restaurant id is displayed",restaurantIds);
         socket.connect();
         socket.on('hello', () => {
             console.log("In hello");
@@ -30,18 +30,43 @@ $j(document).ready(function () {
                 client: 'web',
                 version: '2',
             });
+            console.log("The orders has been printed")
         });
 
         socket.on('res_order', data => {
             console.log("Received order")
-            fetch('https://pos.appscomp.in/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
+            console.log("the data is printed", data.tabId)
+            io.emit(data.tabId)
+            fetch('https://www.zomato.com/merchant-api/orders/order-details?tab_id=', data.tabId)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("The response data is here", data)
+                fetch('https://pos.appscomp.in/get/zomato', {
+                    method: 'POST',
+                    mode: 'no-cors',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                })
+                console.log("these data's has been send to Odoo Server")
+            });
+            console.log("End of the function")
         });
+//        socket.on('res_order', data => {
+//            console.log("Received order")
+//            fetch('https://pos.appscomp.in/get/zomato', {
+//                method: 'POST',
+//                mode: 'no-cors',
+//                headers: {
+//                    'Content-Type': 'application/json',
+//                },
+//                body: JSON.stringify(data),
+//            })
+//            console.log("the data is printed", JSON.stringify(data),data)
+//        });
+//        setTimeout(getZomato, 5000);
+//        console.log("The 5 seconds condition is working")
     }
 
     function getSwiggy() {
@@ -49,7 +74,7 @@ $j(document).ready(function () {
             .then((response) => response.json())
             .then((data) => {
                 console.log("The response data is here", data)
-                fetch('https://pos.appscomp.in/api/login', {
+                fetch('https://pos.appscomp.in/get/swiggy', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -57,6 +82,8 @@ $j(document).ready(function () {
                     body: JSON.stringify(data),
                 })
             });
+//        setTimeout(getZomato, 5000);
+//        console.log("The 5 seconds condition is working")
     }
 
     const url = location.href;
